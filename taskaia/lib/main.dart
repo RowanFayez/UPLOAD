f2b51d5c6cdcb2b68c5ad7f5244306f15b76028b
RowanFayez <fayezrowan5@gmail.com>
2025-07-26 02:01:36 +0200

    clean up last stupid codes

--- taskaia/lib/main.dart
+++ taskaia/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TaskAIA',
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      
     );
   }
 }
